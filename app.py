7e57bdc93459ffac1adf8b17dd83b976e22b9ffd
chytl <119913848+Cahyatul@users.noreply.github.com>
2025-08-27 19:02:39 +0700

    Update app.py

--- app.py
+++ app.py
@@ -47,8 +47,8 @@ class ModifiedLeNet(nn.Module):
 # Load model
 @st.cache_resource
 def ModifiedLeNet():
-    model = LeNet(num_classes=4)
-    model.load_state_dict(torch.load("LeNet_model.pth", map_location=torch.device('cpu')))
+    model = ModifiedLeNet(num_classes=4)
+    model.load_state_dict(torch.load("ModifiedLeNet_model.pth", map_location=torch.device('cpu')))
     model.eval()
     return model
 
